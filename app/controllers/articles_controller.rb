89e0aba7972ddec7630f4e4dabf3902d3512ce13
kumakumagai <fuifuic@hotmail.co.jp>
2017-09-04 21:13:24 +0900

    Article用のアクションとビューを作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,29 @@
 class ArticlesController < ApplicationController
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+    redirect_to action: :index
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    redirect_to action: :index
+  end
+
+  def destroy
+    redirect_to action: :index
+  end
 end
