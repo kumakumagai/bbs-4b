db4dd242328411bf64564086a33de479f3024056
kumakumagai <fuifuic@hotmail.co.jp>
2017-09-04 21:17:43 +0900

    カテゴリー、タグ、コメントのアクションとビューを作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,29 @@
 class CommentsController < ApplicationController
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+    redirect_to action: :index
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    redirect_to action: :index
+  end
+
+  def destroy
+    redirect_to action: :index
+  end
 end
