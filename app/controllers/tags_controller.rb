e1c62c27370d4a4bdcc0f11f5359845f48a4a972
kumakumagai <fuifuic@hotmail.co.jp>
2017-09-15 20:04:08 +0900

    タグ作成機能を追加

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,26 @@
 class TagsController < ApplicationController
+  def index
+    @tags = Tag.page(params[:page])
+  end
+
+  def new
+    @tag = Tag.new
+  end
+
+  def create
+    @tag = Tag.new(tag_params)
+    @tag.save!
+
+    flash[:success] = 'タグを作成しました。'
+
+    redirect_to action: :index
+  rescue
+    render 'new'
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:name)
+  end
 end
