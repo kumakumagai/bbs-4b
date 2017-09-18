39406cfff8fa37b2ccff7fa0aba7512cd8014109
kumakumagai <fuifuic@hotmail.co.jp>
2017-09-18 17:58:00 +0900

    コメント登録処理を作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,20 @@
 class CommentsController < ApplicationController
+  def create
+    comment = Comment.new(comment_params)
+    comment.article_comment = ArticleComment.new(article_id: params[:article_id], comment_id: comment_params[:id])
+
+    comment.save!
+
+    flash[:success] = 'コメントしました。'
+  rescue
+    flash[:error] = 'コメントに失敗しました。'
+  ensure
+    redirect_to article_path(params[:article_id])
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:username, :body)
+  end
 end
